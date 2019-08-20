f95cc7e8dac6ab897531f0613d1212e862062e56
cuitaixiang <754060645@qq.com>
2019-08-20 17:58:07 +0800

    add menu

--- app/src/main/java/tx/investment_statement/MainActivity.java
+++ app/src/main/java/tx/investment_statement/MainActivity.java
@@ -2,12 +2,41 @@ package tx.investment_statement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG, "Hello world!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item: {
+                Toast.makeText(this, "You click add!", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.delete_item: {
+                Toast.makeText(this, "You click delete!", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default:
+        }
+        return true;
     }
 }
